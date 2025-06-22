97cfcce96cbae094ba665d5b12ee9e838060a763
George Pantazopoulos <georgepantaz94@gmail.com>
2025-06-22 11:15:17 +0100

    feat: add main app

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+
+import gradio as gr
+from huggingface_hub import login
+from playground_app import demo as playground_tab
+
+auth_token = os.environ.get("HF_TOKEN", None)
+if auth_token:
+    login(token=auth_token)
+
+
+title = """
+<div align="center">
+    <span>Tokenization Playground</span>
+</div>
+"""
+
+with gr.Blocks() as demo:
+    _ = gr.HTML(f"<h1 style='text-align: center; margin-bottom: 1rem'>{title}</h1>")
+    _ = playground_tab.render()
+
+if __name__ == "__main__":
+    # demo.launch()
+    demo.launch(share=True)
